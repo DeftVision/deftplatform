5e83b5ae3f4dfc898114a5df5d37d91ce885c49f
Gabriel <gjbarret@asu.edu>
2024-07-03 16:14:34 -0600

    generic form structure

---
+++ frontend/src/components/GenericFormStructure.js
@@ -0,0 +1,19 @@
+import {Box, Checkbox, TextField, Typography} from '@mui/material';
+
+import React from 'react';
+
+const GenericFormStructure = ({form_fields}) => {
+    return (
+        <div>
+           {/* {form_fields.map((form_field => (
+                <TextField/>
+                <TextField />
+                <Checkbox />            ))}
+
+
+*/}
+        </div>
+    );
+};
+
+export default GenericFormStructure;
--- frontend/src/components/NavDrawer.js
+++ frontend/src/components/NavDrawer.js
@@ -1,7 +1,6 @@
 import {
     AppBar,
     Box,
-    Button,
     CssBaseline,
     Divider,
     Drawer,
@@ -33,23 +32,52 @@ import {
     WbSunny
 } from '@mui/icons-material';
 import LogoutIcon from '@mui/icons-material/Logout';
-import {useState, useContext} from 'react';
+import { useState, useContext } from 'react';
 import UserContext from '../components/UserContext';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import cookies from "js-cookie";
 
 const drawerWidth = 240;
+const miniDrawerWidth = 60;
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+    ({ theme, open }) => ({
+        flexGrow: 1,
+        padding: theme.spacing(3),
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        marginLeft: `-${miniDrawerWidth}px`,
+        ...(open && {
+            transition: theme.transitions.create('margin', {
+                easing: theme.transitions.easing.easeOut,
+                duration: theme.transitions.duration.enteringScreen,
+            }),
+            marginLeft: 0,
+        }),
+    }),
+);
+
 const DrawerHeader = styled('div')(({ theme }) => ({
     display: 'flex',
-}))
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+}));
 
-export default function NavDrawer({toggleTheme, theme}) {
+export default function NavDrawer({ toggleTheme, theme }) {
     const [open, setOpen] = useState(false);
-    const {user, setUser} = useContext(UserContext);
+    const { user, setUser } = useContext(UserContext);
 
-    const toggleDrawer = (newOpen) => () => {
-        setOpen(newOpen);
-    }
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
 
     function logout() {
         setUser(null);
@@ -57,126 +85,155 @@ export default function NavDrawer({toggleTheme, theme}) {
     }
 
     return (
-        <Box sx={{display: 'flex'}}>
+        <Box sx={{ display: 'flex' }}>
             <CssBaseline />
-            <AppBar position="fixed" open={open}>
+            <AppBar
+                position="fixed"
+                sx={{
+                    zIndex: (theme) => theme.zIndex.drawer,
+                    transition: (theme) => theme.transitions.create(['width', 'margin'], {
+                        easing: theme.transitions.easing.sharp,
+                        duration: theme.transitions.duration.leavingScreen,
+                    }),
+                    marginLeft: open ? drawerWidth : miniDrawerWidth,
+                    width: `calc(100% - ${open ? drawerWidth : miniDrawerWidth}px)`
+                }}
+            >
                 <Toolbar>
                     <IconButton
-                        onClick={toggleDrawer(true)}
-                        sx={{
-                            marginRight: 5,
-                            ...(open && { display: 'none'})
-                        }}
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        onClick={handleDrawerOpen}
+                        sx={{ marginRight: 2, ...(open && { display: 'none' }) }}
                     >
-                        <Menu/>
+                        <Menu />
                     </IconButton>
+                    <Typography variant="h6" noWrap component="div">Your Application</Typography>
                 </Toolbar>
             </AppBar>
 
             <Drawer
+                variant="permanent"
+                sx={{
+                    width: open ? drawerWidth : miniDrawerWidth,
+                    flexShrink: 0,
+                    whiteSpace: 'nowrap',
+                    boxSizing: 'border-box',
+                    '& .MuiDrawer-paper': {
+                        width: open ? drawerWidth : miniDrawerWidth,
+                        transition: (theme) => theme.transitions.create('width', {
+                            easing: theme.transitions.easing.sharp,
+                            duration: theme.transitions.duration.enteringScreen,
+                        }),
+                        overflowX: 'hidden',
+                    },
+                }}
                 open={open}
-                onClose={toggleDrawer(false)}
             >
-                <List sx={{alignItems: 'center'}}>
-                <Typography sx={{textAlign: 'center', marginTop: 2, marginBottom: 2}}>User Name</Typography>
-                    <Divider />
-
-
+                <DrawerHeader>
+                    <IconButton onClick={handleDrawerClose}>
+                        {theme.direction === 'ltr' ? <ChevronRight /> : <ChevronLeft />}
+                    </IconButton>
+                </DrawerHeader>
+                <Divider />
+                <List>
+                    <Typography sx={{ textAlign: 'center', marginTop: 2, marginBottom: 2 }}>{user?.name || 'User Name'}</Typography>
                     <ListItem disablePadding>
                         <ListItemButton component={Link} to="/">
                             <ListItemIcon>
-                                <Home/>
+                                <Home />
                             </ListItemIcon>
-                            <ListItemText primary="Home"/>
+                            <ListItemText primary="Home" />
                         </ListItemButton>
                     </ListItem>
                     <ListItem disablePadding>
                         <ListItemButton component={Link} to="/dashboard">
                             <ListItemIcon>
-                                <Insights/>
+                                <Insights />
                             </ListItemIcon>
-                            <ListItemText primary="Dashboard"/>
+                            <ListItemText primary="Dashboard" />
                         </ListItemButton>
                     </ListItem>
                     <ListItem disablePadding>
                         <ListItemButton component={Link} to="/evaluations">
                             <ListItemIcon>
-                                <Leaderboard/>
+                                <Leaderboard />
                             </ListItemIcon>
-                            <ListItemText primary="Evaluation"/>
+                            <ListItemText primary="Evaluation" />
                         </ListItemButton>
                     </ListItem>
                     <ListItem disablePadding>
                         <ListItemButton component={Link} to="/documents">
                             <ListItemIcon>
-                                <LocalLibrary/>
+                                <LocalLibrary />
                             </ListItemIcon>
-                            <ListItemText primary="Documents"/>
+                            <ListItemText primary="Documents" />
                         </ListItemButton>
                     </ListItem>
                     <ListItem disablePadding>
                         <ListItemButton component={Link} to="/users">
                             <ListItemIcon>
-                                <Person/>
+                                <Person />
                             </ListItemIcon>
-                            <ListItemText primary="Users"/>
+                            <ListItemText primary="Users" />
                         </ListItemButton>
                     </ListItem>
                     <ListItem disablePadding>
                         <ListItemButton component={Link} to="/announcements">
                             <ListItemIcon>
-                                <Announcement/>
+                                <Announcement />
                             </ListItemIcon>
-                            <ListItemText primary="Announcements"/>
+                            <ListItemText primary="Announcements" />
                         </ListItemButton>
                     </ListItem>
                     <ListItem disablePadding>
                         <ListItemButton component={Link} to="/tickets">
                             <ListItemIcon>
-                                <Help/>
+                                <Help />
                             </ListItemIcon>
-                            <ListItemText primary="Support"/>
+                            <ListItemText primary="Support" />
                         </ListItemButton>
                     </ListItem>
-
                     {user && user.role === 'Admin' && (
-                    <ListItem disablePadding>
-                        <ListItemButton component={Link} to="/admin">
-                            <ListItemIcon>
-                                <ShieldRounded />
-                            </ListItemIcon>
-                            <ListItemText primary="Administration"/>
-                        </ListItemButton>
-                    </ListItem>
+                        <ListItem disablePadding>
+                            <ListItemButton component={Link} to="/admin">
+                                <ListItemIcon>
+                                    <ShieldRounded />
+                                </ListItemIcon>
+                                <ListItemText primary="Administration" />
+                            </ListItemButton>
+                        </ListItem>
                     )}
-
-                    {user &&
-                    <ListItem disablePadding>
-                        <ListItemButton component={Link} onClick={logout}>
-                            <ListItemIcon>
-                                <LogoutIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Logout"/>
-                        </ListItemButton>
-                    </ListItem>}
+                    {user && (
+                        <ListItem disablePadding>
+                            <ListItemButton component={Link} onClick={logout}>
+                                <ListItemIcon>
+                                    <LogoutIcon />
+                                </ListItemIcon>
+                                <ListItemText primary="Logout" />
+                            </ListItemButton>
+                        </ListItem>
+                    )}
                     <ListItem>
                         <FormControlLabel
-                            control={<Switch
-                                checked={theme === 'dark'}
-                                onChange={toggleTheme}
-                                icon={<Brightness1/>}
-                                checkedIcon={<WbSunny/>}
-                            />}
-                            label=""
+                            control={
+                                <Switch
+                                    checked={theme === 'dark'}
+                                    onChange={toggleTheme}
+                                    icon={<Brightness1 />}
+                                    checkedIcon={<WbSunny />}
+                                />
+                            }
+                            label="Mode"
                         />
-                        <ListItemText primary="Mode"/>
-
                     </ListItem>
                 </List>
-
             </Drawer>
-
+            <Main open={open}>
+                <DrawerHeader />
+                {/* Rest of your content */}
+            </Main>
         </Box>
     );
 };
-
